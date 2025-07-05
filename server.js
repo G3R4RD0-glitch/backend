b5f5b56386d55464effd8f1c300c592a97d49340
G3R4RD0-glitch <gerardoaa.ti22@utsjr.edu.mx>
2025-07-05 15:47:11 -0600

    fix api cancionRoutes

--- server.js
+++ server.js
@@ -1,47 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');  
-const bodyParser = require('body-parser');  
-const createError = require('http-errors'); 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
 
 // Conexión a MongoDB
 mongoose.connect('mongodb+srv://gerardoaati22:D0n0hMDPFUNkptPz@cluster0.fj5zru8.mongodb.net/canciones01?retryWrites=true&w=majority&appName=Cluster0')
-.then((x) => {
+  .then((x) => {
     console.log(`Conectado exitosamente a la BD: "${x.connections[0].name}"`)
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.error('Error al conectar a la base de datos:', error.reason)
-});
+  });
 
-// Configuración del servidor
 const musicaRutas = require('./routes/musica.routes.js');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// 👉 Ruta base para evitar el error "Not Found"
+app.get('/', (req, res) => {
+  res.send('🎶 API de Canciones en línea');
+});
 
 app.use('/api', musicaRutas);
 
 // Puerto
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-    console.log('Servidor escuchando en el puerto ' + port);
+  console.log('Servidor escuchando en el puerto ' + port);
 });
 
 // Manejador de error 404
 app.use((req, res, next) => {
-    next(createError(404));
+  next(createError(404));
 });
 
 // Manejador de errores generales
 app.use((err, req, res, next) => {
-    console.log(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
+  console.log(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
 });
